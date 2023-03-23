125fdbf5d16b46f9079f8aa895bbeaf3b497d2ad
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2023-03-23 14:10:29 -0600

    scripts.js se agrega

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+function countdown() {
+    const cursoInicio = new Date("2023-04-21").getTime();
+    const ahora = new Date().getTime();
+    const diferencia = cursoInicio - ahora;
+
+    const segundos = 1000;
+    const minutos = segundos * 60;
+    const horas = minutos * 60;
+    const dias = horas * 24;
+
+    const diasRestantes = Math.floor(diferencia / dias);
+
+    document.getElementById("countdown").innerHTML = "Iniciamos el <strong> 21 de abril de 2023</strong>: <br>faltan " + diasRestantes + " días.</br >";
+}
+
+countdown();
