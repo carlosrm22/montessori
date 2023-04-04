9cffcd26adf2b4cd3e4b2cbc2be1b387d543f46e
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2023-04-04 16:06:56 -0600

    se agrega nueva masterclass

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,6 @@
 // contador de días
 function countdown() {
-    const cursoInicio = new Date("2023-04-21").getTime();
+    const cursoInicio = new Date("2023-04-22").getTime();
     const ahora = new Date().getTime();
     const diferencia = cursoInicio - ahora;
 
@@ -15,3 +15,26 @@ function countdown() {
 }
 
 countdown();
+
+// Contador para la masterclass
+function actualizarContador() {
+    const divContador = document.getElementById('contadorMasterClass');
+    const fechaEvento = new Date('2023-04-08T10:00:00'); // Cambia esta fecha por la de tu evento
+    const fechaActual = new Date();
+    const diferencia = fechaEvento - fechaActual;
+
+    if (diferencia < 0) {
+        divContador.innerHTML = 'El evento ya ha comenzado';
+        clearInterval(intervalo);
+        return;
+    }
+
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+
+    divContador.innerHTML = `Faltan ${dias} días, ${horas} horas y ${minutos} minutos para la clase, regístrate:`;
+}
+
+const intervalo = setInterval(actualizarContador, 1000);
+actualizarContador();
